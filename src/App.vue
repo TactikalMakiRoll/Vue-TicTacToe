13ca7496604b0d4d9501d0caf3a5687c5d27fbf3
TactikalMakiRoll <sagridin@gmail.com>
2022-12-16 00:50:20 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <main class="app">
+    <h1 v-if=!field_size class="app__title"><img src="./assets/icons8-vue-js.svg" alt="Vue"> Tic Tac Toe</h1>
+    <div v-if=!field_size class="options">
+      <h2 class="options__description">
+        A simple Tic Tac Toe app build with Vue 3 Composition API
+      </h2>
+      <h2 class="options__choose">
+        Choose the number of cells
+      </h2>
+      <button @click="()=>{field_size = 3}" class="options__size-btn">3x3</button>
+      <button @click="()=>{field_size = 5}" class="options__size-btn">5x5</button>
+      <button @click="()=>{field_size = 7}" class="options__size-btn">7x7</button>
+    </div>
+    <GameComponentVue v-if="field_size" :field_size="field_size" />
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import GameComponentVue from "./components/GameComponent.vue";
+
+const field_size = ref(null)
+
+
+</script>
+
+<style>
+  .app{
+    text-align: center;
+    padding: 2rem 2rem 0 2rem;
+  }
+  .app__title{
+    font-size:3em;
+    font-weight: bold;
+  }
+  .options{
+    padding: 2rem 0;
+  }
+  .options__description{
+    font-size:1.1em;
+  }
+  .options__choose{
+    font-size: 1.25em;
+    margin: 2rem 0 2rem 0;
+  }
+  .options__size-btn{
+    appearance: none;
+    border: dashed 3px white;
+    color:white;
+    font-weight: bold;
+    font-size:1.2em;
+    padding: 0.5rem 1.25rem;
+    background-color:rgba(0,0,0,0);
+  }
+  .options__size-btn:hover{
+    cursor:pointer;
+    background-color:rgb(196, 125, 38);
+  }
+  .options__size-btn + .options__size-btn{
+    margin-left:1.5rem;
+  }
+</style>
---
+++ src/components/GameComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="game">
+        <div :style="fieldStyle " class="field">
+            <div ref="fieldArr" @click="makeMove(cellNum)" v-for="cellNum in field_size*field_size" :key="cellNum" class="field__cell">
+                <img v-if="fieldArray[cellNum] === 'x'" src="../assets/icons8-multiply-80.png" alt="X">
+                <img v-if="fieldArray[cellNum] === 'o'" src="../assets/icons8-o-80.png" alt="O">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {ref, defineProps, onMounted, computed} from "vue";
+
+const props = defineProps(["field_size"]);
+
+let fieldArray = ref(null);
+fieldArray.value = [];  
+
+const fieldStyle = computed(() => {
+    let cellsFr = "1fr ".repeat(props.field_size);
+    return {'grid-template-columns': cellsFr.slice(0,cellsFr.length-1),
+            'grid-template-rows': cellsFr.slice(0,cellsFr.length-1)}
+})
+
+const fieldArr = ref();
+
+const makeMove = function(cellNumber){
+    cellNumber;
+}
+
+
+onMounted(()=>{
+    // fieldArray.value = [];
+    // for(let i=0;i<props.field_size;i++){
+    //     let row = [].fill(" ",0,props.field_size);
+    //     fieldArray.value.push(row); 
+    // }
+})
+
+</script>
+
+<style>
+    .game{
+        padding:2rem 0;
+    }
+    .field{
+        display: grid;
+        min-width: calc(12rem + 8px);
+        max-width: min(50vw,75vh);
+        aspect-ratio: 1;
+        margin: 0 auto;
+
+        border: 4px dashed white;
+    }
+    .field__cell{
+        border: 2px solid white;
+        width: 100%;
+        height: 100%; 
+        min-width:4rem;
+        min-height:4rem;
+        aspect-ratio: 1;
+        object-fit: fill;
+    }
+</style>
